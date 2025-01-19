133b13376acdbb705cc08b2475136af2b1f0debc
Kabillan T A <kabillan1905@gmail.com>
2025-01-20 03:05:21 +0530

    added sklearn version for logistic regression

--- logisticregression.py
+++ logisticregression.py
@@ -14,7 +14,7 @@ data = pd.DataFrame(dataset)
 X = data[['Feature1', 'Feature2']]
 y = data['Label']                  
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
@@ -58,3 +58,34 @@ def predict(X, w, b):
     z = np.dot(X, w) + b
     y_pred = sigmoid(z)
     return (y_pred >= 0.5).astype(int)
+
+X_train_np = X_train.to_numpy()
+y_train_np = y_train.to_numpy()
+
+w, b = train_logistic_regression(X_train_np, y_train_np, lr=0.1, epochs=1000)
+
+
+X_test_np = X_test.to_numpy()
+y_test_np = y_test.to_numpy()
+
+y_pred = predict(X_test_np, w, b)
+
+accuracy = accuracy_score(y_test_np, y_pred)
+print(f"Accuracy: {accuracy}")
+
+y_pred_proba = sigmoid(np.dot(X_test_np, w) + b)
+log_loss_value = log_loss(y_test_np, y_pred_proba)
+print(f"Log Loss: {log_loss_value}")
+
+
+
+# Above is the mathematical intituion for better understanding lol :((
+# Here is the same implemented using sklearn :))
+
+from sklearn.linear_model import LogisticRegression
+
+model = LogisticRegression()
+model.fit(X_train,y_train)
+skpred = model.predict(X_test)
+skaccuracy = accuracy_score(y_test, skpred)
+print(skaccuracy)
