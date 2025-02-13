0f233732fae16fc6f914d368b103eee114a8e604
Kabillan T A <kabillan1905@gmail.com>
2025-02-13 15:19:18 +0530

    update

--- neuralnetwork_tf.py
+++ neuralnetwork_tf.py
@@ -3,17 +3,13 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.datasets import mnist
 
-# Load the MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Normalize the images to a range of 0 to 1
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# Flatten the images to row vectors
 x_train = x_train.reshape(-1, 784)
 x_test = x_test.reshape(-1, 784)
 
-# Define the model
 model = Sequential([
     Dense(10, activation='relu', input_shape=(784,)),  
     Dense(10, activation='relu'),                      
@@ -28,3 +24,4 @@ model.fit(x_train, y_train, epochs=5, batch_size=32)
 
 loss, accuracy = model.evaluate(x_test, y_test)
 print(f"Loss: {loss}, Accuracy: {accuracy}")
+
